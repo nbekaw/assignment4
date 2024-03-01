55e6c8890c1245d66ea4b336fc4c5d73ce01b0f1
sagynyshbola <154623393+sagynyshbola@users.noreply.github.com>
2024-03-02 01:15:38 +0600

    Update UserController.java

--- EndTerm/src/main/java/kz/aitu/endterm/controllers/UserController.java
+++ EndTerm/src/main/java/kz/aitu/endterm/controllers/UserController.java
@@ -1,6 +1,7 @@
 package kz.aitu.endterm.controllers;
 
 import kz.aitu.endterm.models.User;
+import kz.aitu.endterm.services.UserService;
 import kz.aitu.endterm.services.interfaces.UserServiceInterface;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +13,11 @@ import java.util.List;
 @RequestMapping
 public class UserController {
     private final UserServiceInterface service;
+    private final UserService userService;
 
-    public UserController(UserServiceInterface service) {
+    public UserController(UserServiceInterface service, UserService userService) {
         this.service = service;
+        this.userService = userService;
     }
 
     @GetMapping("/users/")
@@ -30,7 +33,40 @@ public class UserController {
         }
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
+    @PostMapping("/withdraw/{senderUsername}/{ReceiverUsername}/{amount}")
+    public ResponseEntity<String> withdrawMoneyBetweenUsers(@PathVariable String senderUsername, @PathVariable String ReceiverUsername, @PathVariable double amount) {
+        User sender = userService.findByUsername(senderUsername);
+        User receiver = userService.findByUsername(ReceiverUsername);
 
+        if (sender == null || receiver == null) {
+            return new ResponseEntity<>("Sender or receiver not found", HttpStatus.NOT_FOUND);
+        }
 
+        if (amount <= 0) {
+            return new ResponseEntity<>("Invalid amount", HttpStatus.BAD_REQUEST);
+        }
+
+        if (sender.getBalance() < amount) {
+            return new ResponseEntity<>("Insufficient balance", HttpStatus.BAD_REQUEST);
+        }
+
+        sender.withdraw(amount);
+        receiver.setBalance(receiver.getBalance() + amount);
+
+        userService.update(sender);
+        userService.update(receiver);
+
+        return new ResponseEntity<>("Money transferred successfully", HttpStatus.OK);
+    }
 }
+
+
